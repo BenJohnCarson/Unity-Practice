e555c473cea2be2406eb14973e43537622ba6fc4
BenJohnCarson <benjohncarson@gmail.com>
2017-03-16 22:03:54 +0000

    Ui button to place turrets

--- Assets/BuildManager.cs
+++ Assets/BuildManager.cs
@@ -18,15 +18,15 @@ public class BuildManager : MonoBehaviour {
 
 	public GameObject standardTurretPrefab;
 
-	void Start ()
-	{
-		turretToBuild = standardTurretPrefab;
-	}
-
 	private GameObject turretToBuild;
 
 	public GameObject GetTurretToBuild()
 	{
 		return turretToBuild;
+	}
+
+	public void SetTurretToBuild(GameObject turret)
+	{
+		turretToBuild = turret;
 	}
 }
